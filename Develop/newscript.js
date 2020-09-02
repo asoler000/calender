b423848151a8ee58acaf12a1143eba7c0251cfe5
asoler000 <68714690+asoler000@users.noreply.github.com>
2020-09-02 05:33:32 -0500

    Update newscript.js
    New script using jquery and java

--- Develop/newscript.js
+++ Develop/newscript.js
@@ -1,35 +1,59 @@
-var NowMoment = moment();
-var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('MMMM Do YYYY, h:mm:ss a');
+$(document).ready(function(){
 
+  var eDisplayMoment = document.getElementById('currentDay');
+  eDisplayMoment.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+  
+  
 
-var todoInput = document.querySelector("#9AM");
-var todoForm = document.querySelector("#10AM");
-var todoList = document.querySelector("#11AM");
-var todoInput = document.querySelector("#12PM");
-var todoForm = document.querySelector("#1PM");
-var todoList = document.querySelector("#2PM");
-var todoList = document.querySelector("#3PM");
-var todoList = document.querySelector("#4PM");
-var todoList = document.querySelector("#5PM");
-
-function init() {
+  var currentHour = moment().hours()
+  
+  $('.timeblock').each(function () {
+    var blockHour = $(this).attr('id')
+  
+    if (blockHour < currentHour) {
+      $(this).addClass('.past')
+  
+    } else if (blockHour === currentHour) {
+      $(this).removeClass('.past')
+      $(this).addClass('.present')
+  
+    } else {
+  
+    }$(this).addClass('.future')
+  
+  })
+  
+  
+  function init() {
     // Get stored todos from localStorage
     // Parsing the JSON string to an object
-    var timeNotes = JSON.parse(localStorage.getItem("9AM"));
-    var timeNotes = JSON.parse(localStorage.getItem("10AM"));
-    var timeNotes = JSON.parse(localStorage.getItem("11AM"));
-    var timeNotes = JSON.parse(localStorage.getItem("12PM"));
-    var timeNotes = JSON.parse(localStorage.getItem("1PM"));
-    var timeNotes = JSON.parse(localStorage.getItem("2PM"));
-    var timeNotes = JSON.parse(localStorage.getItem("3PM"));
-    var timeNotes = JSON.parse(localStorage.getItem("4PM"));
-    var timeNotes = JSON.parse(localStorage.getItem("5PM"));
-  
+    var timeNotesNine = JSON.parse(localStorage.getItem("9AM"));
+    var timeNotesTen = JSON.parse(localStorage.getItem("10AM"));
+    var timeNotesEleven = JSON.parse(localStorage.getItem("11AM"));
+    var timeNotesTwelve = JSON.parse(localStorage.getItem("12PM"));
+    var timeNotesOne = JSON.parse(localStorage.getItem("1PM"));
+    var timeNotesTwo = JSON.parse(localStorage.getItem("2PM"));
+    var timeNotesThree = JSON.parse(localStorage.getItem("3PM"));
+    var timeNotesFour = JSON.parse(localStorage.getItem("4PM"));
+    var timeNotesFive = JSON.parse(localStorage.getItem("5PM"));
+
+    var timeNotes = [timeNotesNine, timeNotesTen, timeNotesEleven, timeNotesTwelve, timeNotesOne, timeNotesTwo, timeNotesThree, timeNotesFour, timeNotesFive]
     // If todos were retrieved from localStorage, update the todos array to it
-    if (timeNotes !== null) {
-      todos = timeNotes;
+    for (myelement of timeNotes) {
+      if (myelement !== null) {
+        todos.push(myelement)
+      }
     }
+  
+    $('#9AM .todo-Description').val(localStorage.getItem("9AM"));
+    $('#10AM .todo-Description').val(localStorage.getItem("10AM"));
+    $('#11AM .todo-Description').val(localStorage.getItem("11AM"));
+    $('#12AM .todo-Description').val(localStorage.getItem("12AM"));
+    $('#1PM .todo-Description').val(localStorage.getItem("1PM"));
+    $('#2PM .todo-Description').val(localStorage.getItem("2PM"));
+    $('#3PM .todo-Description').val(localStorage.getItem("3PM"));
+    $('#4PM .todo-Description').val(localStorage.getItem("4PM"));
+    $('#5PM .todo-Description').val(localStorage.getItem("5PM"));
   
     // Render todos to the DOM
     render9AM();
@@ -48,8 +72,10 @@ function init() {
     localStorage.setItem("todos", JSON.stringify(todos));
   }
   
+  
+  
   // When form is submitted...
-  todoForm.addEventListener("submit", function(event) {
+  todoForm.addEventListener("submit", function (event) {
     event.preventDefault();
   
     var todoText = todoInput.value.trim();
@@ -67,3 +93,10 @@ function init() {
     storeTodos();
     renderTodos();
   });
+  function renderTodos() {
+    for (element of todos) {
+      console.log(element)
+    }
+  
+  }
+})
